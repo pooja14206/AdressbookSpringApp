ea7c55e52fcb68918f016b7ce6a959842ff4b7b7
Pooja14206 <pooja.14206@gmail.com>
2021-08-14 08:41:21 +0530

    Added a file for Adressbook App

---
+++ AddressbookSpringApp/src/main/java/com/org/AddressbookSpringApp/AddressbookSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.org.AddressbookSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbookSpringAppApplication.class, args);
+		System.out.println("Welcome To Addressbook App.");
+	}
+}
